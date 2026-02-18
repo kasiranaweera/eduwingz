d2097e60dfe5afd4ec65663f3a3f9a9678e6f336
kasiranaweera <ranaweerakasi@gmail.com>
2026-02-18 20:31:50 +0530

    chore: configure deployment to use backend/backend folder only

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -e
 
 # Navigate to backend directory and build
 cd backend/backend
-pip install -r ../../requirements.txt
+pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --no-input
